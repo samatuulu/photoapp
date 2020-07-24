88bcea4fa278a50ffc5e0f721eb368c41f95f6d4
Beka <>
2020-07-24 17:12:50 +0600

    Creates model Photo and updates requirements.txt, also other few configurations related to image

--- source/photos/models.py
+++ source/photos/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+STATUS_CHOICES = (
+    ('publish', 'Publish'),
+    ('unpublish', 'Unpublish')
+)
+
+
+class Photo(models.Model):
+    photo = models.ImageField(upload_to='uploads')
+    caption = models.TextField(max_length=2000)
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=40, choices=STATUS_CHOICES, default=STATUS_CHOICES[0][0])
+
+    def __str__(self):
+        return str(self.id)
+
+    class Meta:
+        ordering = ('-created_at',)
